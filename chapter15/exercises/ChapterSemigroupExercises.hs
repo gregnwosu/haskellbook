0f54cdb415fe6db989bdf99915ffbdda8eb202e6
Greg Nwosu <greg.nwosu@gmail.com>
2016-05-14 10:51:52 +0100

    end of chapter 15

--- chapter15/exercises/ChapterSemigroupExercises.hs
+++ chapter15/exercises/ChapterSemigroupExercises.hs
@@ -6,12 +6,43 @@ import Data.Semigroup
 import Test.QuickCheck
 
 
-monoidAssoc :: (Semigroup a, Eq a) => a -> a -> a -> Bool
-monoidAssoc a b c = (a <> b)  <> c == (a <> (b <> c))
+instance Arbitrary BoolConj where
+  arbitrary = BoolConj <$> arbitrary
 
+instance Arbitrary BoolDisj where
+  arbitrary = BoolDisj <$> arbitrary
+
+newtype Mem s a = Mem {
+  runMem :: s -> (a,s)
+}
+
+instance Monoid a => Monoid (Mem s a) where
+  (Mem f) `mappend` (Mem f') = Mem $ \x -> let (a, s)   = f x
+                                               (a', s') = f' s
+                                           in (mappend a a',s')
+  mempty = Mem $ \s -> (mempty,s)
+
+f' :: (Num s ) => (Mem Int String)
+f' = Mem $ \s -> ("hi", s+1)
+
+semigroupAssoc :: (Semigroup a, Eq a) => a -> a -> a -> Bool
+semigroupAssoc a b c = (a <> b)  <> c == (a <> (b <> c))
+
+monoidLeftIdentity :: (Monoid a, Semigroup a, Eq a) => a -> Bool
+monoidLeftIdentity a =  mempty <> a == a
+
+monoidRightIdentity :: (Monoid a, Semigroup a , Eq a) => a -> Bool
+monoidRightIdentity a = a <> mempty == a
 
 data Trivial  = Trivial deriving (Eq, Show)
 data Or a b = Fst a | Snd b deriving (Eq, Show)
+
+instance Monoid Trivial where
+  mempty = Trivial
+  mappend = (<>)
+
+instance (Arbitrary a) => Arbitrary (Comp a) where
+  arbitrary =  Comp <$> const <$> arbitrary
 
 
 instance Semigroup (Or a b) where
@@ -27,11 +58,20 @@ newtype BoolDisj  = BoolDisj Bool deriving (Eq, Show)
 newtype Combine a b = Combine {unCombine :: a -> b}
 instance Semigroup BoolConj  where
   (BoolConj x) <> (BoolConj y) = BoolConj $ x && y
+instance Monoid BoolConj where
+  mappend = (<>)
+  mempty = BoolConj True
+instance Monoid BoolDisj where
+  mappend = (<>)
+  mempty = BoolDisj False
 
 instance Semigroup BoolDisj  where
   (BoolDisj x) <> (BoolDisj y) = BoolDisj $ x || y
 instance Semigroup b => Semigroup (Combine a b) where
  (Combine f) <> (Combine g) =  Combine $  \a -> f a <> g a
+instance (Semigroup b, Monoid b) =>  Monoid (Combine a b) where
+  mappend  = (<>)
+  mempty = Combine $ const mempty
 
 newtype Three a b c= Three (a, b, c)
  deriving (Eq, Show)
@@ -64,11 +104,18 @@ instance (Arbitrary b) => Arbitrary (Two a b) where
   arbitrary = liftM Two arbitrary
 instance (Semigroup b) => Semigroup (Two a b) where
   (Two b) <> (Two b') = Two $ b <> b'
+instance (Semigroup b , Monoid b) => Monoid (Two a b) where
+  (Two b) `mappend` (Two b')= Two (b <> b')
+  mempty  = Two mempty
 instance Semigroup Trivial where
   _ <> _ = Trivial
 
 instance (Semigroup a) => Semigroup (Identity a) where
    (Identity a ) <> (Identity b) = Identity $ a <> b
+
+instance (Semigroup a , Monoid a) => Monoid (Identity a) where
+  mappend  = (<>)
+  mempty = Identity (mempty)
 instance Arbitrary Trivial where
   arbitrary = return Trivial
 instance Arbitrary a => Arbitrary (Identity a) where
@@ -95,6 +142,9 @@ instance (Num a) => Semigroup (MySum a) where
 instance Semigroup (Comp a) where
   f <> g = Comp $ unComp f .  unComp g
 
+instance Monoid (Comp a) where
+  mappend = (<>)
+  mempty = Comp id
 data Validation a b = Failure' a | Success' b
   deriving (Eq, Show)
 
@@ -154,13 +204,26 @@ instance Semigroup a =>
 
 main :: IO()
 main = do
-  quickCheck (monoidAssoc :: Trivial -> Trivial -> Trivial -> Bool)
-  quickCheck (monoidAssoc :: Identity String-> Identity String -> Identity String -> Bool)
-  quickCheck (monoidAssoc :: Two () String-> Two () String -> Two ()String -> Bool)
-  quickCheck (monoidAssoc :: Thr -> Thr -> Thr -> Bool)
-  quickCheck (monoidAssoc :: Fore -> Fore -> Fore -> Bool)
-  quickCheck (monoidAssoc :: Val -> Val -> Val -> Bool)
-  quickCheck (monoidAssoc :: AccR -> AccR -> AccR -> Bool)
-  quickCheck (monoidAssoc :: AccB -> AccB -> AccB -> Bool)
-  quickCheck $ bools (\(a,b,c) -> monoidAssoc (BoolConj a) (BoolConj b) (BoolConj c))
-  quickCheck $ bools (\(a,b,c) -> monoidAssoc (BoolDisj a) (BoolDisj b) (BoolDisj c))
+  quickCheck (semigroupAssoc :: Trivial -> Trivial -> Trivial -> Bool)
+  quickCheck (monoidRightIdentity :: Trivial -> Bool)
+  quickCheck (monoidLeftIdentity :: Trivial -> Bool)
+  quickCheck (semigroupAssoc :: Identity String-> Identity String -> Identity String -> Bool)
+  quickCheck (monoidRightIdentity :: Identity String-> Bool)
+  quickCheck (monoidLeftIdentity :: Identity String-> Bool)
+  quickCheck (semigroupAssoc :: Two () String-> Two () String -> Two ()String -> Bool)
+  quickCheck (monoidRightIdentity :: Two Int String-> Bool)
+  quickCheck (monoidLeftIdentity :: Two Int String-> Bool)
+  quickCheck (semigroupAssoc :: Thr -> Thr -> Thr -> Bool)
+  quickCheck (semigroupAssoc :: Fore -> Fore -> Fore -> Bool)
+  quickCheck (semigroupAssoc :: Val -> Val -> Val -> Bool)
+  quickCheck (semigroupAssoc :: AccR -> AccR -> AccR -> Bool)
+  quickCheck (semigroupAssoc :: AccB -> AccB -> AccB -> Bool)
+  quickCheck $ bools (\(a,b,c) -> semigroupAssoc (BoolConj a) (BoolConj b) (BoolConj c))
+  quickCheck $ bools (\(a,b,c) -> semigroupAssoc (BoolDisj a) (BoolDisj b) (BoolDisj c))
+  quickCheck (monoidRightIdentity :: BoolConj -> Bool)
+  quickCheck (monoidLeftIdentity :: BoolConj -> Bool)
+  print $ runMem (f' `mappend` mempty) 0
+  print $ runMem (mempty `mappend` f') 0
+  print  (runMem mempty 0 :: (String, Int))
+  print $ runMem (f' `mappend` mempty) 0 == runMem f' 0
+  print $ runMem (mempty `mappend` f') 0 == runMem f' 0
