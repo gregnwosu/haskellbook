cf0ef5fb6b70ba483556737d548fd0ff7e838ac6
Greg Nwosu <greg.nwosu@gmail.com>
2016-05-15 15:56:41 +0100

    more studying of fmap.fmap typechecking

--- chapter16/Examples.hs
+++ chapter16/Examples.hs
@@ -13,12 +13,54 @@ instance Functor FixMePls  where
   fmap _ FixMe = FixMe
   fmap f (Pls a) = Pls (f a)
 
--- fmap :: Functor f => (a -> b) -> f a -> f b
--- fmap :: Functor g => (b -> c) -> g b -> g c
--- (fmap.fmap) :: (Functor f, Functor f1) => (a -> b) -> f (f1 a) -> f (f1 b)
--- (.)  :: ((a -> b) -> f a -> f b) -> ((b -> c) -> g b -> g c) -> a -> c
--- (.) = f (g x)
--- (.) =
+{-
+
+ courtesy of http://stackoverflow.com/questions/23030638/how-fmap-fmap-typechecks
+First let's change the type variables' names to be unique:
+
+(.)  :: (a -> b) -> (r -> a) -> (r -> b)
+fmap :: Functor f => (c -> d) -> f c -> f d
+fmap :: Functor g => (x -> y) -> g x -> g y
+Now the first parameter to . has type a -> b and we supply an argument of type (c -> d) -> (f c -> f d), so a is c -> d and b is f c -> f d. So so far we have:
+
+(.) :: Functor f => -- Left operand
+                    ((c -> d) -> (f c -> f d)) ->
+                    -- Right operand
+                    (r -> (c -> d)) ->
+                    -- Result
+                    (r -> (f c -> f d))
+The second parameter to . has type r -> a a.k.a. r -> (c -> d) and the argument we give has type (x -> y) -> (g x -> g y), so r becomes x -> y, c becomes g x and d becomes g y. So now we have:
+
+(.)       :: (Functor f, Functor g) => -- Left operand
+                                       ((g x -> g y) -> (f (g x) -> f (g y))) ->
+                                       -- Right operand
+                                       ((x -> y) -> (g x -> g y)) ->
+                                       -- Result
+                                       (x -> y) -> f (g x) -> f (g y)
+fmap.fmap :: (Functor f, Functor g) => (x -> y) -> f (g x) -> f (g y)
+
+-}
+
+{- (fmap.fmap) :: (Functor f, Functor f1) => (a -> b) -> f (f1 a) -> f (f1 b)
+ fmap :: Functor f => (i -> j) -> f i -> f j
+ fmap :: Functor g => (x -> y) -> g x -> g y
+
+ (.) :: (b -> c) -> (a -> b) -> a -> c
+ substituting in fmaps
+   b = (g x -> g y)
+   c = (f g x -> f g y)
+   a = x -> y
+   i = g x
+   j = g y
+  -- substituting back into compose
+. = ((g x -> g y)  -> (f (g x) -> f (g y)) ->
+    ((x -> y) -> (g x -> g y)) ->
+    (x -> y)  -> f(g x) -> f (g y)
+-- applying the  first two arguments (fmap) gives
+
+ fmap . fmap =     (x -> y)  -> f(g x) -> f (g y)
+
+
 
 -- courtesy of http://adit.io/posts/2013-07-22-lenses-in-pictures.html
 -- “If you want to do function composition where a function has two arguments”, says Sherlock, “you need (.).(.)!”
@@ -36,7 +78,7 @@ instance Functor FixMePls  where
 -- (.).(.) :: (b -> c) -> (a1 -> a2 -> b) -> (a1 -> a2 -> c)
 -- fmap . fmap :: (a -> b) -> f (f1 a) -> f (f1 b)
 -- There’s a pattern happening here: fmap . fmap and (.) . (.) both allow us to go “one level deeper”. In fmap it means going inside one more layer of functors. In function composition your functor is r ->, so it means you can pass in one more argument to your function.
-
+-}
 data WhoCares a = ItDoesnt | Matter a | WhatThisIsCalled
   deriving (Eq, Show)
 
