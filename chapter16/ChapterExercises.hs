4ed45c64660cb685e40dc892efa507e029aab35d
Greg Nwosu <greg.nwosu@gmail.com>
2016-05-17 18:17:41 +0100

    chapter16 complete

--- chapter16/ChapterExercises.hs
+++ chapter16/ChapterExercises.hs
@@ -117,3 +117,14 @@ instance Functor (GoatLord) where
   fmap _ NoGoat = NoGoat
   fmap f (OneGoat a) = OneGoat $ f a
   fmap f (MoreGoats x y z) = MoreGoats (fmap f x) (fmap f y) (fmap f z)
+
+
+data TalkToMe a =
+  Halt |
+  Print String a |
+  Read (String -> a)
+
+instance Functor TalkToMe where
+  fmap _ Halt = Halt
+  fmap f (Print s a) = Print s $ f a
+  fmap f (Read x) = Read $ fmap f x
