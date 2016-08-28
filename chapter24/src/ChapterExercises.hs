87ad6d8dca2674480eb902097a53092745135115
greg <greg.nwosu@gmail.com>
2016-08-28 12:43:22 +0100

    question 5 parsing

--- chapter24/src/ChapterExercises.hs
+++ chapter24/src/ChapterExercises.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module ChapterExercises where
 
 import Data.Char
@@ -98,9 +100,16 @@ parsePhone = PhoneNumber <$> parseNumberPlan <*> parseExchange <*> parseLineNumb
 -------------------------------------------------- question 5
 type CommentEntry = String
 type Activity = String
-data ActivityLine = ActivityLine TimeOfDay Activity (Maybe CommentEntry)
-data LogEntry = DayLogEntry Day [ActivityLine] | CommentLogEntry CommentEntry
-data LogFile = LogFile [LogEntry]
+
+data ActivityLine =
+    ActivityLine TimeOfDay Activity  CommentEntry
+    deriving (Show)
+data LogEntry =
+    DayLogEntry Day [ActivityLine] | CommentLogEntry CommentEntry
+    deriving (Show)
+data LogFile =
+     LogFile [LogEntry]
+     deriving (Show)
 
 parseCommentEntry :: Parser CommentEntry
 parseCommentEntry = string "-- " *> some (notChar '\n')
@@ -108,11 +117,42 @@ parseTimeOfDay :: Parser TimeOfDay
 parseTimeOfDay = TimeOfDay <$> (fromIntegral <$> integer) <* char ':' <*> (fromIntegral <$> integer) <*> pure 0
 
 parseActivityLine :: Parser ActivityLine
-parseActivityLine = ActivityLine <$> parseTimeOfDay <* char ' ' <*> some anyChar <* notFollowedBy (string "--") <*> optional parseCommentEntry
- 
+parseActivityLine = ActivityLine
+                    <$> (parseTimeOfDay )
+                    <*> (some anyChar) -- <* notFollowedBy (string "--"))
+                    <*> (option "" parseCommentEntry)
+
 parseDay :: Parser Day
 parseDay = fromGregorian <$> (toInt <$> count 4 digit) <* char '-' <*> (fromIntegral . toInt <$> count 2 digit) <* char '-' <*> (fromIntegral . toInt <$> count 2 digit)
-                    
+
 parseLogEntry :: Parser LogEntry
-parseLogEntry = (CommentLogEntry <$> parseCommentEntry) <|>
-                (DayLogEntry <$> (string "# " *> parseDay)  <*> some parseActivityLine) 
+parseLogEntry = ((CommentLogEntry <$> parseCommentEntry) <|>
+                (DayLogEntry <$> (string "# " *> parseDay <* newline)  <*> some parseActivityLine)) <* (pure () <$> newline <|> eof) 
+parseLogFile :: Parser LogFile
+parseLogFile = LogFile <$> some parseLogEntry
+
+exampleLog :: String
+exampleLog = [r|-- wheee a comment
+# 2025-02-05
+08:00 Breakfast
+09:00 Sanitizing moisture collector
+11:00 Exercising in high-grav gym
+12:00 Lunch
+13:00 Programming
+17:00 Commuting home in rover
+17:30 R&R
+19:00 Dinner
+21:00 Shower
+21:15 Read
+22:00 Sleep
+# 2025-02-07 -- dates not nececessarily sequential
+08:00 Breakfast -- should I try skippin bfast?
+09:00 Bumped head, passed out
+13:36 Wake up, headache
+13:37 Go to medbay
+13:40 Patch self up
+13:45 Commute home for rest
+14:15 Read
+21:00 Dinner
+21:15 Read
+22:00 Sleep|]
