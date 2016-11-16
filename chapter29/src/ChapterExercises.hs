6c5e6b9608f85908f6e80968a8cc1288da5a6ce2
greg <greg.nwosu@gmail.com>
2016-11-16 19:54:24 +0000

    more exercises

--- chapter29/src/ChapterExercises.hs
+++ chapter29/src/ChapterExercises.hs
@@ -1,19 +1,49 @@
-module ChapterExercises where
+module Main where
 
 import Data.Char
-makeNum = ord . toUpper
-spaceShiftStream :: (Int -> Int -> Int) -> String -> String -> String
-spaceShiftStream f ks (' ':ts) = ' ':spaceShiftStream f ks ts
-spaceShiftStream f ks [] = []
-spaceShiftStream f (k:ks) (t:ts) = let baseChr = subtract 65 . makeNum . toUpper
-                                       textnum = baseChr t
-                                       keynum = baseChr k
-                                       shiftAmount = textnum `f` keynum  `mod` 26
-                                   in chr ( 65 + shiftAmount ) : spaceShiftStream f ks ts
+import System.Environment
+import System.IO
 
+baseChr :: Char -> Int
+baseChr = subtract 65 . ord . toUpper
+
+transform :: (Int -> Int -> Int) -> String -> String -> String
+transform f ks (' ':ts) = ' ':transform f ks ts
+transform f _ [] = []
+transform f (k:ks) (t:ts) = let textnum = baseChr t
+                                keynum = baseChr k
+                                shiftAmount = textnum `f` keynum  `mod` 26
+                            in chr ( 65 + shiftAmount ) : transform f ks ts
 
 vigenère :: String -> String -> String
-vigenère key   = spaceShiftStream (+) ( concat $ repeat key)
+vigenère key   = transform (+) ( concat $ repeat key)
 
 unvigenère :: String -> String -> String
-unvigenère key   = spaceShiftStream (-) ( concat $ repeat key)
+unvigenère key   = transform (-) ( concat $ repeat key)
+
+key = "ALLY"
+forArg ("d": _) = unvigenère key
+forArg ("e":_) = vigenère key
+
+timeoutMillis :: [String] -> IO Int
+timeoutMillis (_:t:_) = return $ read t
+timeoutMillis  _ =
+    putStr "\ntimeout not supplied default to 5000 ms" >>
+    return 5000
+nextChar :: Char -> IO String
+nextChar c = isEOF >>= go
+             where go True = return []
+                   go False = (c:) <$> readStdInTillEOF
+
+
+readStdInTillEOF :: IO String
+readStdInTillEOF = do
+  c <- getChar
+  nextChar c
+
+main :: IO ()
+main = do
+         crypt <- forArg <$> getArgs
+         timeout <-  getArgs >>= timeoutMillis
+         isTimedOut <- hWaitForInput stdin timeout
+         if (isTimedOut)  then  ( putStrLn " ">> readStdInTillEOF >>= print . crypt)  else (putStrLn "\ntimed out waiting for input")
